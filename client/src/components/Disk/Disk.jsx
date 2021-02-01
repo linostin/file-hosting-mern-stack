3ee8bd865aacdc1c66fc94920b43a58f143ad8e0
linostin <lifesins@gmail.com>
2021-02-02 00:04:35 +0300

    DragAndDrop (front) ready

--- client/src/components/Disk/Disk.jsx
+++ client/src/components/Disk/Disk.jsx
@@ -6,10 +6,13 @@ import FileList from "./FileList/FileList";
 import Popup from "./Popup/Popup";
 import UploadButton from './UploadButton/UploadButton'
 
+import "./Styles/style.css"
+
 function Disk() {
   const dispatch = useDispatch();
 
   const [popupOpen, setPopupOpen] = useState(false);
+  const [dragDropEnter, setDragDropEnter] = useState(false)
   
   // const isAuth = useSelector((state) => state.user.isAuth);
   const currentDir = useSelector((state) => state.files.currentDir);
@@ -52,8 +55,28 @@ function Disk() {
     files.forEach(file => dispatch(uploadFile(file, currentDir)))
   }
 
-  return (
-    <div className="disk">
+  const dragEnterHandler = (event) => {
+    event.preventDefault()
+    event.stopPropagation()
+    setDragDropEnter(true)
+  }
+
+  const dragLeaveHandler = (event) => {
+    event.preventDefault()
+    event.stopPropagation()
+    setDragDropEnter(false)
+  }
+
+  const dropHandler = (event) => {
+    event.preventDefault()
+    event.stopPropagation()
+    let files = [...event.dataTransfer.files]
+    files.forEach(file => dispatch(uploadFile(file, currentDir)))
+    setDragDropEnter(false)
+  }
+
+  return ( !dragDropEnter ?
+    <div className="disk" onDragEnter={dragEnterHandler} onDragLeave={dragLeaveHandler} onDragOver={dragEnterHandler}>
       <div className="disk__btns">
         <button className="disk__back" onClick={backFolderFunc}>Назад</button>
         <button className="disk__create" onClick={() => popupOpenFunc()}>
@@ -70,6 +93,10 @@ function Disk() {
         popupCloseFunc={popupCloseFunc}
         createDirHandler={createDirHandler}
       />
+    </div>
+    :
+    <div className="drag__and__drop" onDrop={dropHandler} onDragEnter={dragEnterHandler} onDragLeave={dragLeaveHandler} onDragOver={dragEnterHandler}>
+      Перетащите файлы сюда
     </div>
   );
 }
